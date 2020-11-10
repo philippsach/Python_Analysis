310a74305b2783e69a9585d7114f252ffbf55890
Philipp Sach <philippsach@Philipps-MacBook-Pro.local>
2020-11-10 17:30:52 +0100

    commit

---
+++ connect_database.py
@@ -0,0 +1,7 @@
+import mysql.connector
+
+
+#con = mysql.connector.connect(user = "phil_sach",
+#                             password = "entthesis2020",
+#                            host = "85.214.204.221:3306",
+#                           database = )
