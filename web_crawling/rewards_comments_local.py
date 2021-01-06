6a58e8f6519d122597e06b16391e789fb6fef522
Philipp Sach <philipp.sach@gmx.net>
2021-01-06 20:10:06 +0100

    fixed problem with being detected as non-human: added argument user_agent to options of chrome driver

--- web_crawling/rewards_comments_local.py
+++ web_crawling/rewards_comments_local.py
@@ -16,6 +16,19 @@ import lxml.etree as et
 
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
+headers = {
+    'authority': 'scrapeme.live',
+    'dnt': '1',
+    'upgrade-insecure-requests': '1',
+    'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 11_1_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36',
+    'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+    'sec-fetch-site': 'none',
+    'sec-fetch-mode': 'navigate',
+    'sec-fetch-user': '?1',
+    'sec-fetch-dest': 'document',
+    'accept-language': 'en-GB,en-US;q=0.9,en;q=0.8',
+}
+
 mp.set_start_method('spawn', force=True)
 __spec__ = "ModuleSpec(name='builtins', loader=<class '_frozen_importlib.BuiltinImporter'>)"
 #print(mp.get_start_method())
@@ -64,6 +77,7 @@ def crawl_comments(link, pr_nr, path, n_comments_sql):
     
     options = webdriver.ChromeOptions()
     options.add_argument("headless")
+    options.add_argument("user-agent=Mozilla/5.0 (Macintosh; Intel Mac OS X 11_1_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36")
     driver = webdriver.Chrome(executable_path="/Users/philippsach/.conda/envs/Python_Analysis/bin/chromedriver",
                               options=options)
     driver.set_window_position(2000, 0)
